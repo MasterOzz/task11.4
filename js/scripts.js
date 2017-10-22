2e7e61716b0e2540a2f6dadf476b82a0e21615e3
Piotr Szczepanik <piotrszczepanik29@wp.pl>
2017-10-22 22:50:32 +0200

    pliki

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+﻿function Phone(brand, price, color) {
+    this.brand = brand;
+	this.price = price;
+	this.color = color;
+}
+Phone.prototype.printInfo = function() {
+		console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
+}
+
+var iPhone6S = new Phone("Apple", 2250, "silver");
+iPhone6S.printInfo();
+
+var SamsungGalaxyS6 = new Phone("Samsung", 1099, "zloty");
+SamsungGalaxyS6.printInfo();
+
+var OnePlusOne = new Phone("OnePlus", 1299, "black");
+OnePlusOne.printInfo();
